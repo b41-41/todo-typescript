4b187e2a8a636dc8bfc00d9f7a6169071f9cdad8
b41-41 <41@b41.kr>
2022-02-01 18:01:29 +0900

    Style - 기본 마크업 및 스타일링
    - 할 일 목록 앱의 기본 틀을 마크업
    - 마크업된 기본 틀을 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, ChangeEvent, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
+
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodo] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+  };
+
   return (
-    <></>
+    <div className='App'>
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" placeholder="Task..." onChange={handleChange}/>
+          <input type="number" placeholder="Deadline (in Days)..." onChange={handleChange} />
+        </div>
+          <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
+    </div>
   );
 }
 
